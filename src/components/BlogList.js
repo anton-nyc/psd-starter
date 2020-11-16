d83295e57e045d9b909562fc85094d3a8b54ead9
fatt <fatt@fatt.com>
2020-11-16 18:28:26 -0500

    added dynamic page creation

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,16 +1,22 @@
 import React from 'react';
+
 import { graphql, useStaticQuery } from 'gatsby';
+
 import BlogPost from './BlogPost';
+
 export default function BlogList() {
   const data = useStaticQuery(graphql`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             id
             frontmatter {
               title
               date(formatString: "MMMM D, YYYY")
+            }
+            fields {
+              slug
             }
             excerpt
           }
@@ -19,14 +25,16 @@ export default function BlogList() {
     }
   `);
 
-    return (
+  return (
     <div>
       {data.allMarkdownRemark.edges.map(edge => (
         <BlogPost
           key={edge.node.id}
+          slug={edge.node.fields.slug}
           title={edge.node.frontmatter.title}
           date={edge.node.frontmatter.date}
-          excerpt={edge.node.excerpt} />
+          excerpt={edge.node.excerpt}
+        />
       ))}
     </div>
   );
